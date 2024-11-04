f643c4fc092f22655c656e40e4cac8c926789e6d
PedroFerreiraLopes <jpedro.serrita@gmail.com>
2024-11-04 19:13:38 -0300

    BookConstants top and bot screens dead center y position

--- BookConstants.lua
+++ BookConstants.lua
@@ -3,6 +3,9 @@ local table = {};
 table.pokedex = {
     x = 285 + (371/2),
     y = 215 + (835/2),
+    yTopScreen = 215 + (835/2) 
+        - 178,
+    yBottomScreen = 848,
     xScale = 371/500,
     yScale = 835/1125,
 }
